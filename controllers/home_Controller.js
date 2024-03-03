cafa41529581d82e462dc528cfd52f159e5cee8f
sahil sayyad <igsahilsayyad@gmail.com>
2024-03-04 02:47:11 +0530

    order history page done

--- controllers/home_Controller.js
+++ controllers/home_Controller.js
@@ -1,4 +1,7 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
+const User = require("../models/user");
+const Address = require("../models/address");
 module.exports.home = async (req, res) => {
   try {
     const products = await Product.find({});
@@ -28,8 +31,49 @@ module.exports.about = async (req, res) => {
 };
 module.exports.profile = async (req, res) => {
   try {
+
+    let user = await User.findById(req.user._id)
+      .populate("cart")
+      .populate({
+        path: "cart",
+        populate: {
+          path: "product",
+          module: "Product",
+        },
+      })
+      .populate("address");
+    let address = await Address.find({});
+    let length = 0;
+    let addressinfo;
+    if (address) {
+      addressinfo = user.address;
+      if (addressinfo) {
+        length = Object.keys(addressinfo).length;
+      }
+    }
+
+    const cartinfo = user.cart;
+    let subTotal = 0;
+    cartinfo.forEach((item) => {
+      let price = parseFloat(item.product.price);
+      let quantity = parseFloat(item.quantity);
+      if (!isNaN(price) && !isNaN(quantity)) {
+        subTotal = subTotal + price * quantity;
+      }
+    });
+    let shippingFee = 100;
+    if (subTotal === 0) {
+      shippingFee = 0;
+    }
+    let noShow = true;
     return res.render("profile", {
       title: "Anime Aura | Profile",
+      cartinfo,
+      shippingFee,
+      subTotal,
+      noShow,
+      addressinfo,
+      length,
     });
   } catch (err) {
     console.log(`error in home controller ${err}`);
