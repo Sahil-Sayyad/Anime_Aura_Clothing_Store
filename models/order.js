db67bd2e37b01d120e9eebd9145a007dc78ee944
Software Developer <96423459+Sahil-Sayyad@users.noreply.github.com>
2024-03-15 00:19:56 +0530

    Update order.js

--- models/order.js
+++ models/order.js
@@ -1,29 +1,35 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    customername:{
-        type:String,
-        required:true
+    customername: {
+        type: String,
+        required: true
     },
-    orderdate:{
-        type:String,
-        required:true
+    orderdate: {
+        type: String,
+        required: true
     },
-    totalAmount:{
-        type:String,
-        required:true
+    totalAmount: {
+        type: String,
+        required: true
     },
-    product:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Product'
+    billing: [{
+        quantity: {
+            type: String,
+            required: true,
+        },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product'
+        },
     }],
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     }
-},{
-    timestamps:true
+}, {
+    timestamps: true
 });
 
-const Order = mongoose.model('Order',orderSchema);
+const Order = mongoose.model('Order', orderSchema);
 module.exports = Order;
